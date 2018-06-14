ef1b5123b5eaf3281913c06e32dc54a15e67baef
John Biesnecker <jbiesnecker@gmail.com>
2018-06-14 14:56:01 -0700

    Updated a bit, especially argument parsing

--- dice.hs
+++ dice.hs
@@ -1,19 +1,50 @@
-module Main where
-import System.Random
-import System.Environment
-import Control.Monad
+{-# LANGUAGE RecordWildCards #-}
 
-handleArgs :: [String] -> [Int]
-handleArgs [x, y] = handleArgs [x, y, "1"]
-handleArgs [x, y, z] = map read [x, y, z]
-handleArgs _ = error "Too few (or many!) arguments."
+module Main
+  ( main
+  ) where
+
+import Options.Applicative
+
+import Control.Monad (replicateM_)
+import Data.Semigroup ((<>))
+import System.Random (newStdGen, RandomGen, randomRs)
+import System.Environment (getArgs)
+
+data Args = Args
+  { n :: Int
+  , m :: Int
+  , times :: Int
+  }
+
+instance Show Args where
+  show Args{..} = unwords $
+    [ "Rolling"
+    , show n ++ "d" ++ show m
+    , show times
+    , "time(s)"
+    ]
+
+diceArgs :: Parser Args
+diceArgs = Args
+  <$> argument auto (metavar "N" <> value 1)
+  <*> argument auto (metavar "M" <> value 6)
+  <*> argument auto (metavar "TIMES" <> value 1)
+
+opts :: ParserInfo Args
+opts = info (diceArgs <**> helper)
+  (  fullDesc
+  <> progDesc "Roll NdM dice TIMES times"
+  <> header "dice"
+  )
 
 tossDice :: (RandomGen g) => Int -> Int -> g -> [Int]
 tossDice x y = take x . randomRs (1, y)
 
 main :: IO ()
 main = do
-  [x, y, z] <- liftM handleArgs getArgs
-  replicateM_ z $ do
-    r <- liftM (tossDice x y) newStdGen
+  args@Args{..} <- execParser opts
+  putStrLn $ show args
+  replicateM_ times $ do
+    r <- tossDice n m <$> newStdGen
     putStrLn $ "Result: " ++ show (sum r) ++ "\tDice: " ++ show r
